da828722d01e279888ab57306f19b186bd2c5c86
SigmaOnTheSigma <carterwitte434@gmail.com>
2024-11-25 09:27:09 -0500

    Create Change rem name.lua

---
+++ Change rem name.lua
@@ -0,0 +1,7 @@
+local player = game.Players.LocalPlayer
+local rem = player.Backpack["Remington 870"]
+local Name = "shoot the hoes!"
+local ToolTip = "credits to sans and have fun"
+
+rem.Name = Name
+rem.ToolTip = ToolTip
